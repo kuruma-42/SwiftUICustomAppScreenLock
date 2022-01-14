8680f878e078007761890b00c6c65f3951663378
yongjuncha <perfect_yon@naver.com>
2022-01-14 12:06:31 +0900

    Initial Commit

---
+++ AppCustomScreenLockSwiftUI/AppCustomScreenLockSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCustomScreenLockSwiftUIApp.swift
+//  AppCustomScreenLockSwiftUI
+//
+//  Created by Yong Jun Cha on 2022/01/14.
+//
+
+import SwiftUI
+
+@main
+struct AppCustomScreenLockSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppCustomScreenLockSwiftUITests/AppCustomScreenLockSwiftUITests.swift
@@ -0,0 +1,33 @@
+//
+//  AppCustomScreenLockSwiftUITests.swift
+//  AppCustomScreenLockSwiftUITests
+//
+//  Created by Yong Jun Cha on 2022/01/14.
+//
+
+import XCTest
+@testable import AppCustomScreenLockSwiftUI
+
+class AppCustomScreenLockSwiftUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
